069e3abc0bcbef514bd25bb284d29181d5e7af6e
Eda Dursun <edadrsn929@gmail.com>
2025-02-16 20:47:40 +0300

    included bees in the game

--- core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -11,10 +11,14 @@ public class SurvivorBird extends ApplicationAdapter {
     SpriteBatch batch;
     Texture background;
     Texture bird;
+    Texture bee1;
+    Texture bee2;
+    Texture bee3;
     float birdX = 0, birdY = 0;
     int gameState = 0;
     float velocity = 0;
     float gravity = 0.50f;
+    float enemyX=0;
 
     //Proje başladığında çalışacak olan metod
     @Override
@@ -24,6 +28,11 @@ public class SurvivorBird extends ApplicationAdapter {
         bird = new Texture("bird.png");
         birdX = Gdx.graphics.getWidth() / 3 - bird.getHeight() / 2;
         birdY = Gdx.graphics.getHeight() / 3;
+        bee1 = new Texture("bee.png");
+        bee2 = new Texture("bee.png");
+        bee3 = new Texture("bee.png");
+
+        enemyX=900;
 
     }
 
@@ -31,25 +40,30 @@ public class SurvivorBird extends ApplicationAdapter {
     @Override
     public void render() {
 
-
-           if(gameState==1) {
-               if (Gdx.input.justTouched()) {
-                   velocity = -10;
-               }
-
-               if(birdY>0 || velocity<0){
-                   velocity+=gravity;
-                   birdY-=velocity;
-               }
-           } else{
-               if(Gdx.input.justTouched()){
-                   gameState=1;
-               }
-           }
-
-        //Hangi objeleri çizeceğimizi yazıcaz
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if (gameState == 1) {
+            batch.draw(bee1,enemyX,50,Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+            batch.draw(bee2,enemyX,150,Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+            batch.draw(bee3,enemyX,350,Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+            enemyX-=7;
+
+            if (Gdx.input.justTouched()) {
+                velocity = -10;
+            }
+
+            if (birdY > 0 || velocity < 0) {
+                velocity += gravity;
+                birdY -= velocity;
+            }
+        } else {
+            if (Gdx.input.justTouched()) {
+                gameState = 1;
+            }
+        }
+
+        //Hangi objeleri çizeceğimizi yazıcaz
         batch.draw(bird, birdX, birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
         batch.end();
 
