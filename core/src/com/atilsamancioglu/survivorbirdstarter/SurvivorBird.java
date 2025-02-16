fa6825a868dbeb39306d64d1453886547e953df8
Eda Dursun <edadrsn929@gmail.com>
2025-02-16 19:15:31 +0300

    gravity created

--- core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -8,37 +8,58 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SurvivorBird extends ApplicationAdapter {
 
-	SpriteBatch batch;
-	Texture background;
-	Texture bird;
-	float birdX=0,birdY=0;
+    SpriteBatch batch;
+    Texture background;
+    Texture bird;
+    float birdX = 0, birdY = 0;
+    int gameState = 0;
+    float velocity = 0;
+    float gravity = 0.50f;
 
-	//Proje başladığında çalışacak olan metod
-	@Override
-	public void create () {
-		batch=new SpriteBatch();
-		background=new Texture("background.png");
-		bird=new Texture("bird.png");
-		birdX=Gdx.graphics.getWidth()/2-bird.getHeight()/2;
-		birdY=Gdx.graphics.getHeight()/3;
+    //Proje başladığında çalışacak olan metod
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("background.png");
+        bird = new Texture("bird.png");
+        birdX = Gdx.graphics.getWidth() / 2 - bird.getHeight() / 2;
+        birdY = Gdx.graphics.getHeight() / 3;
 
-	}
-	//Oyun döngüsü
-	@Override
-	public void render () {
-		//Hangi objeleri çizeceğimizi yazıcaz
-		batch.begin();
-		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(bird,birdX,birdY,Gdx.graphics.getWidth()/15,Gdx.graphics.getHeight()/10	);
+    }
+
+    //Oyun döngüsü
+    @Override
+    public void render() {
+
+        if (Gdx.input.justTouched()) {
+            gameState = 1;
+        }
+
+        if (gameState == 1) {
+            if (birdY > 0 || velocity < 0) {
+                velocity += gravity;
+                birdY -= velocity;
+            }
+        }
+        else {
+            if (Gdx.input.justTouched()) {
+                gameState = 1;
+            }
+        }
+
+        //Hangi objeleri çizeceğimizi yazıcaz
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(bird, birdX, birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
 
 
-		batch.end();
+        batch.end();
 
-	}
+    }
 
-	//Oyun kapatıldığında çalışacak olan metod
-	@Override
-	public void dispose () {
+    //Oyun kapatıldığında çalışacak olan metod
+    @Override
+    public void dispose() {
 
-	}
+    }
 }
