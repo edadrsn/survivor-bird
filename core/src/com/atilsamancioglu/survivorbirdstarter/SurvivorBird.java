02f1396176a73db388a2f7b4ff0eb4796d3bcc12
Eda Dursun <edadrsn929@gmail.com>
2025-02-16 18:25:52 +0300

    survivor bird app created

---
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -0,0 +1,27 @@
+package com.atilsamancioglu.survivorbirdstarter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class SurvivorBird extends ApplicationAdapter {
+
+
+	//Proje başladığında çalışacak olan metod
+	@Override
+	public void create () {
+
+	}
+	//Oyun döngüsü
+	@Override
+	public void render () {
+
+	}
+
+	//Oyun kapatıldığında çalışacak olan metod
+	@Override
+	public void dispose () {
+
+	}
+}
