94fd511c7cecfdc7629b47767f64d8cddfc7c057
Eda Dursun <edadrsn929@gmail.com>
2025-02-16 19:01:03 +0300

    bird image added

--- core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -11,6 +11,7 @@ public class SurvivorBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture bird;
+	float birdX=0,birdY=0;
 
 	//Proje başladığında çalışacak olan metod
 	@Override
@@ -18,6 +19,8 @@ public class SurvivorBird extends ApplicationAdapter {
 		batch=new SpriteBatch();
 		background=new Texture("background.png");
 		bird=new Texture("bird.png");
+		birdX=Gdx.graphics.getWidth()/2-bird.getHeight()/2;
+		birdY=Gdx.graphics.getHeight()/3;
 
 	}
 	//Oyun döngüsü
@@ -26,6 +29,7 @@ public class SurvivorBird extends ApplicationAdapter {
 		//Hangi objeleri çizeceğimizi yazıcaz
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(bird,birdX,birdY,Gdx.graphics.getWidth()/15,Gdx.graphics.getHeight()/10	);
 
 
 		batch.end();
