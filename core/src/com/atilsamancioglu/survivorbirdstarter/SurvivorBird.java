6798f3d200caaf9d5e1003b13a79421b7c3c3741
Eda Dursun <edadrsn929@gmail.com>
2025-02-16 18:50:01 +0300

    background image added

--- core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -1,21 +1,34 @@
 package com.atilsamancioglu.survivorbirdstarter;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SurvivorBird extends ApplicationAdapter {
 
+	SpriteBatch batch;
+	Texture background;
+	Texture bird;
 
 	//Proje başladığında çalışacak olan metod
 	@Override
 	public void create () {
+		batch=new SpriteBatch();
+		background=new Texture("background.png");
+		bird=new Texture("bird.png");
 
 	}
 	//Oyun döngüsü
 	@Override
 	public void render () {
+		//Hangi objeleri çizeceğimizi yazıcaz
+		batch.begin();
+		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+
+		batch.end();
 
 	}
 
